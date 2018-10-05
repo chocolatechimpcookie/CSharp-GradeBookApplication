7678ef46f59d4ae58827637e48a6c35429d93b0f
Alicina Memar <memaralicina@gmail.com>
2018-10-05 14:00:58 -0400

    Another fix

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,8 +1,10 @@
+using GradeBook.Enums;
+
 namespace GradeBook.GradeBooks
 {
   public class RankedGradeBook : BaseGradeBook
   {
-    public RankedGradeBook : base(name)
+    public RankedGradeBook(string name) : base(name)
     {
       Type = GradeBookType.Ranked;
     }
