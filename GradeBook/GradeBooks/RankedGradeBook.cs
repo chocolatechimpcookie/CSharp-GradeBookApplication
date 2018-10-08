6c56861a90c8367ac3f146f5e57ee3db01f79348
Alicina Memar <memaralicina@gmail.com>
2018-10-08 12:07:09 -0400

    Updating getletter overrddie:

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,5 +8,30 @@ namespace GradeBook.GradeBooks
     {
       Type = GradeBookType.Ranked;
     }
+    public override GetLetterGrade(double averageGrade)
+    {
+      if(Students.Count < 5)
+      {
+        throw new InvalidOperationException("Minimum of 5 students needed");
+
+      }
+      else
+      {
+        if (averageGrade >= 90)
+            return 'A';
+        else if (averageGrade >= 80)
+            return 'B';
+        else if (averageGrade >= 70)
+            return 'C';
+        else if (averageGrade >= 60)
+            return 'D';
+        else
+            return 'F';
+      }
+    }
+
+
+
   }
+
 }
